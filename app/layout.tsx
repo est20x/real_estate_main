4b670b723635c5378eab17a44d0c0bcf75361a15
DenysM <113093199+est20x@users.noreply.github.com>
2026-04-16 15:54:51 +0300

    Add Facebook Pixel tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 
 const gilroy = localFont({
@@ -63,12 +64,14 @@ export const viewport: Viewport = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ru" className={`${gilroy.variable} h-full antialiased`}>
-       <head>
+      <body className="min-h-full flex flex-col m-0 p-0">
+        
+        {/* Facebook Pixel */}
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -77,14 +80,16 @@ export default function RootLayout({
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
             n.queue=[];t=b.createElement(e);t.async=!0;
             t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
             fbq('init', '728688463665547');
             fbq('track', 'PageView');
           `}
         </Script>
+
+        {/* Fallback для no-js */}
         <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             height="1"
             width="1"
@@ -93,8 +98,9 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
-      </head>
-      <body className="min-h-full flex flex-col m-0 p-0">{children}</body>
+
+        {children}
+      </body>
     </html>
   );
 }
